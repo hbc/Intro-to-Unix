336593ca0c7283612d96b84e183a6ffdd70d25c1
rkhetani <khetani.r@gmail.com>
2016-05-13 15:02:47 -0400

    Update rnaseq_analysis_on_file.sh

--- rnaseq_analysis_on_file.sh
+++ rnaseq_analysis_on_file.sh
@@ -5,7 +5,11 @@
 mkdir -p results/STAR
 
 # Run STAR
-STAR --runThreadN 6 --genomeDir /groups/hbctraining/unix_oct2015_other/reference_STAR --readFilesIn data/trimmed_fastq/Mov10_oe_1.subset.fq.qualtrim25.minlen35.fq  --outFileNamePrefix results/STAR/Mov10_oe_1_ --outFilterMultimapNmax 10 --outSAMstrandField intronMotif --outReadsUnmapped Fastx --outSAMtype BAM SortedByCoordinate --outSAMunmapped Within --outSAMattributes NH HI NM MD AS
+STAR --runThreadN 6 --genomeDir /groups/hbctraining/unix_oct2015_other/reference_STAR \
+--readFilesIn data/trimmed_fastq/Mov10_oe_1.subset.fq.qualtrim25.minlen35.fq  \
+--outFileNamePrefix results/STAR/Mov10_oe_1_ --outFilterMultimapNmax 10 --outSAMstrandField intronMotif \
+--outReadsUnmapped Fastx --outSAMtype BAM SortedByCoordinate --outSAMunmapped Within \
+--outSAMattributes NH HI NM MD AS
 
 # Create BAM index
 samtools index results/STAR/Mov10_oe_1__Aligned.sortedByCoord.out.bam
@@ -14,4 +18,5 @@ samtools index results/STAR/Mov10_oe_1__Aligned.sortedByCoord.out.bam
 mkdir -p results/counts
 
 # Count mapped reads
-htseq-count --stranded reverse --format bam results/STAR/Mov10_oe_1_Aligned.sortedByCoord.out.bam data/reference_data/chr1-hg19_genes.gtf  >  results/counts/Mov10_oe_1.counts
+htseq-count --stranded reverse --format bam results/STAR/Mov10_oe_1_Aligned.sortedByCoord.out.bam \
+data/reference_data/chr1-hg19_genes.gtf  >  results/counts/Mov10_oe_1.counts
