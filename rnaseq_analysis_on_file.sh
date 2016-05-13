e225864690a9c084cb7e409cddf0404600df7867
rkhetani <khetani.r@gmail.com>
2016-05-13 15:01:18 -0400

    Update rnaseq_analysis_on_file.sh

--- rnaseq_analysis_on_file.sh
+++ rnaseq_analysis_on_file.sh
@@ -5,7 +5,7 @@
 mkdir -p results/STAR
 
 # Run STAR
-STAR --runThreadN 6 --genomeDir /groups/hbctraining/unix_oct2015_other/reference_STAR --readFilesIn data/trimmed_fastq/Mov10_oe_1.qualtrim25.minlen35.fq  --outFileNamePrefix results/STAR/Mov10_oe_1_ --outFilterMultimapNmax 10 --outSAMstrandField intronMotif --outReadsUnmapped Fastx --outSAMtype BAM SortedByCoordinate --outSAMunmapped Within --outSAMattributes NH HI NM MD AS
+STAR --runThreadN 6 --genomeDir /groups/hbctraining/unix_oct2015_other/reference_STAR --readFilesIn data/trimmed_fastq/Mov10_oe_1.subset.fq.qualtrim25.minlen35.fq  --outFileNamePrefix results/STAR/Mov10_oe_1_ --outFilterMultimapNmax 10 --outSAMstrandField intronMotif --outReadsUnmapped Fastx --outSAMtype BAM SortedByCoordinate --outSAMunmapped Within --outSAMattributes NH HI NM MD AS
 
 # Create BAM index
 samtools index results/STAR/Mov10_oe_1__Aligned.sortedByCoord.out.bam
