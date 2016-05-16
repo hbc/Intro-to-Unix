cbc613997e731615f88d21c79e6b67ba39308790
rkhetani <khetani.r@gmail.com>
2016-05-16 11:00:38 -0400

    Update rnaseq_analysis_on_allfiles_for-lsf.sh

--- rnaseq_analysis_on_allfiles_for-lsf.sh
+++ rnaseq_analysis_on_allfiles_for-lsf.sh
@@ -1,7 +1,8 @@
-#!/bin/bash
+#! /bin/bash
 
-for fq in ~/unix_workshop/raw_fastq/*.fq
+for fastq in ~/unix_workshop/rnaseq_project/data/trimmed_fastq/*fq
 do
-  bsub -q priority -n 6 -W 1:30 -R "rusage[mem=4000]" -J rnaseq_mov10 -o %J.out -e %J.err "sh ~/rnaseq_analysis_on_allfiles.sh $fq"
-  sleep 1
+bsub -q priority -n6 -W 1:30 -R "rusage[mem=4000]" -J rnaseq_mov10 -o %J.out -e %J.err "sh ~/unix_workshop/rnaseq_project/rnaseq_analysis_on_input_file.sh $fastq"
+sleep 1
 done
+exit
